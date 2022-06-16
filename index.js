1f7f15bc1fada5d49c2895eff6c80712660e07a0
MeeAnn <michalinastudnik@gmail.com>
2022-06-16 12:05:36 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const xd = 'dupa';
+
+/*
+1. shuffle deck of cards
+2. deal the cards between 2 players
+3. play the game: higher value wins, lower value loses, draw = no points for any player
+4. display the players score and result of each round
+5. display who won the game
+6. test the whole thing using unit tests
+ */
