35776e7b3aa03493867534d9996cdd0e759ad8e5
MeeAnn <michalinastudnik@gmail.com>
2022-06-20 15:28:44 +0200

    change 2 MeeAnn's war card game simulator 101: There is a full deck of cards (52 units, w/o jokers) saved in a cardDeck.txt file. The 'wholeDeck' reads the file to an Array and then uses the Fisher-Yates shuffle algorithm to create a playable deck. After that, the deck is dealt evenly between two players (half-half or one-by-one deal). Fight() starts the game:  - each 'round' the prepareCards() checks if the players have cards left to play  - one card is drawn from each player's deck, they are saved to 'pool' ('table content') and compared, and the function returns the difference value to the judge()  - judge() gives a point to a player with a higher card value or starts another deal of cards to resolve the war, it also displays the score each round  - the winner of the round takes all the cards from the table and stores them in the 'poolDeck'. If the player used all his cards, they are passed to his hand after a shuffle  - when one of the players has no more cards left, he loses (comparison of the scores)

--- index.js
+++ index.js
@@ -1,12 +1,129 @@
 'use strict';
 
-const xd = 'some other change';
+const fs = require('fs');
 
-/*
-1. shuffle deck of cards
-2. deal the cards between 2 players
-3. play the game: higher value wins, lower value loses, draw = no points for any player
-4. display the players score and result of each round
-5. display who won the game
-6. test the whole thing using unit tests
- */
+/* - - - - -  T A S K S / R U L E S - - - - -
+1. Create a deck of cards and shuffle it ✓
+2. Deal the cards between 2 players (Half-Half or One-By-One) ✓
+3. Play the game: higher card value wins, lower loses, draw = no points for any player ✓
+4. Display the players score and result of each round ✓
+5. When hand is empty display who won the game ✓
+6. Test the whole thing using unit tests ✗
+*/
+
+// Players:
+const p1 = { startDeck: [], score: 0, poolDeck: [] };
+const p2 = { startDeck: [], score: 0, poolDeck: [] };
+
+// Reads the whole deck from the file
+const path = 'test/cardDeck.txt';
+const wholeDeck = fs.readFileSync(path, 'utf8')
+  .trim()
+  .split('\n')
+  .map(line => line.split(', '));
+
+// Shuffles any array (Fisher–Yates shuffle)
+function shuffle(array) {
+  let currentIndex = array.length; let oldPlacement; let newPlacement;
+
+  while (currentIndex) {
+    newPlacement = Math.floor(Math.random() * currentIndex--);
+
+    oldPlacement = array[currentIndex];
+    array[currentIndex] = array[newPlacement];
+    array[newPlacement] = oldPlacement;
+  }
+  return array;
+}
+
+// Half-half deck deal
+function dealEvenly() {
+  p1.startDeck = deck.slice(0, 26);
+  p2.startDeck = deck.slice(26, 52);
+}
+
+// One-by-One deck deal (no. 1)
+function dealOneByOne() {
+  for (let i = 0; i < 52; i += 2) {
+    p1.startDeck.unshift(deck[i]);
+    p2.startDeck.unshift(deck[i + 1]);
+  }
+}
+
+// One-by-One deck deal (no. 2)
+function dealOneByOneAlt() {
+  for (let i = 0; i < 26; i++) {
+    p1.startDeck.unshift(deck.shift());
+    p2.startDeck.unshift(deck.shift());
+  }
+}
+
+// Checks if player has cards 'in hand', if not, shuffles the 'won cards deck' (if empty - game ends)
+function prepareCards(player) {
+  if (player.startDeck.length === 0 && player.poolDeck.length !== 0) {
+    shuffle(player.poolDeck);
+    while (player.poolDeck.length) {
+      player.startDeck.push(player.poolDeck.pop());
+    }
+  } else if (player.startDeck.length === 0 && player.poolDeck.length === 0) {
+    whoWon();
+    process.exit(0);
+  }
+}
+
+// Takes one card from each players' hand, saves it to the 'win pool' and starts the fight
+function fight() {
+  prepareCards(p1);
+  prepareCards(p2);
+  const p1Card = p1.startDeck.shift();
+  const p2Card = p2.startDeck.shift();
+  pool.push(p1Card, p2Card);
+  console.log(p1Card, 'vs', p2Card);
+  const result = p1Card[2] - p2Card[2];
+  judge(result);
+}
+
+// Judges who won the fight
+function judge(result) {
+  if (result > 0) {
+    console.log('Player 1 scores a point!');
+    p1.score++;
+    console.log(`Score:  ${p1.score} : ${p2.score}`);
+    while (pool.length) {
+      p1.poolDeck.push(pool.pop());
+    }
+    pool.length = 0;
+  } else if (result < 0) {
+    console.log('Player 2 scores a point!');
+    p2.score++;
+    console.log(`Score:  ${p1.score} : ${p2.score}`);
+    while (pool.length) {
+      p2.poolDeck.push(pool.pop());
+    }
+    pool.length = 0;
+  } else if (result === 0) {
+    console.log('This means war!');
+    console.log(`Score:  ${p1.score} : ${p2.score}`);
+    fight();
+  } else console.log('Something went wrong. :(');
+}
+
+// Decides who won the whole game based on score
+function whoWon() {
+  if (p1.score > p2.score) {
+    console.log('Player 1 won!');
+  } else if (p1.score < p2.score) {
+    console.log('Player 2 won!');
+  } else {
+    console.log('Draw, or is it?');
+  }
+}
+
+// - - - - - - G A M E  S T A R T - - - - - - - - //
+const deck = shuffle(wholeDeck);
+const pool = [];
+// dealEvenly();
+dealOneByOne();
+// dealOneByOneAlt();
+for (;;) fight();
+// - - - - - - G A M E  E N D S - - - - - - - - - //
